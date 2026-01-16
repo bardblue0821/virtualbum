b64e5bcb97002134197ce58dc97d26bc9bbacc1c
bardblue0821 <bardblue0821@gmail.com>
2026-01-17 08:16:30 +0900

    不要なファイルの削除

--- .unused_files_backup/HeaderGate.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import { usePathname } from 'next/navigation';
-import Header from './Header';
-
-export default function HeaderGate() {
-  const pathname = usePathname();
-  if (pathname === '/') return null; // Hide header on login page
-  return <Header />;
-}
